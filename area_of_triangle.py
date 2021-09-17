7f43adf04c26d88f0be86876e2c68890382e33f4
Ashutosh <ashu.deshmukh560@gmail.com>
2021-09-17 11:39:09 +0530

    added comments

--- area_of_triangle.py
+++ area_of_triangle.py
@@ -1,12 +1,15 @@
 # Area of triangel
 # where s is the semi-perimeter of the triangle; that is,
 
+#accept 3 input from user
 a = float(input("First :"))
 b = float(input("Second :"))
 c = float(input("Third :"))
 
+
 s = (a + b + c)/2
 
+# According to heron's formula
 area = (s*(s - a)*(s - b)*(s - c)) ** 0.5
 
 print(" The area of triangle is %0.2f " %area)
