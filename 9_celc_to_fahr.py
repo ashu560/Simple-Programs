e5be2bccb3c45ca1f41e1a95d5ef741dfbe13e9a
Ashutosh <ashu.deshmukh560@gmail.com>
2021-09-20 10:12:57 +0530

    C° to F°

---
+++ 9_celc_to_fahr.py
@@ -0,0 +1,12 @@
+# Convert Celcius to Fahrenheit
+
+# C° to F°: Celsius to Fahrenheit Conversion Formula
+# To convert temperatures in degrees Celsius to Fahrenheit, multiply by 1.8 (or 9/5) and add 32
+# fahr = (cels * 9.0/5.0) + 32.0
+
+
+cels = float(input("Enter Temp in Celcius :"))
+
+fahr = (cels * 9.0/5.0) + 32.0
+
+print("Cels to fahr temp is %0.2f F°"  %fahr )
