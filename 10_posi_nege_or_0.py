f39b282520c2693684c4da6c02aed2bbd09304bd
Ashutosh <ashu.deshmukh560@gmail.com>
2021-09-20 10:24:19 +0530

    positive - negative - or 0

---
+++ 10_posi_nege_or_0.py
@@ -0,0 +1,10 @@
+# Check if number is positive - negative or zero
+
+userInput = int(input("Enter a number : "))
+
+if userInput > 0:
+    print("The number is positive.")
+elif userInput < 0:
+    print("The number is Negative.")
+else:
+    print("The number is Zero(0).")
